c64c05f7fe2815e0e4f23c57757aa9abfd1fd8b2
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-03-04 17:46:07 +0330

    end of 15.4

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -12,7 +12,12 @@ console.log('before');
 
 promise.then((data) => {
   console.log('1', data);
-  return 'some data'
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('this is my other promise');
+      // reject('sth went wrong');
+    }, 3000)
+  });
 }).then((str) => {
   console.log('does this run?', str);
 }).catch((error) => {
