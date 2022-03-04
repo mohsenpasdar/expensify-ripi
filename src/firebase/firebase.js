c23315f8077f18abc154d05634e5a001403d9872
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-03-04 19:49:07 +0330

    end of 15.5

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -9,13 +9,13 @@ import 'firebase/functions';   // for cloud functions
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD4UXYqK1zlcBmrTwi8BCfK_Ashjd7luL0",
-  authDomain: "expensify-c09ad.firebaseapp.com",
-  databaseURL: "https://expensify-c09ad-default-rtdb.firebaseio.com",
-  projectId: "expensify-c09ad",
-  storageBucket: "expensify-c09ad.appspot.com",
-  messagingSenderId: "967668925786",
-  appId: "1:967668925786:web:f360cf4356520ca8299f63"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
