f6fdc5bbd567dba9957ca145a81b3771ee2b5a42
mohsenpasdar <66749029+mohsenpasdar@users.noreply.github.com>
2022-03-08 00:50:03 +0330

    in the middle of 15.8'

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -39,3 +39,62 @@ export const editExpense = (id, updates) => ({
   id,
   updates
 });
+
+// SET_EXPENSES
+export const setExpenses = (expenses) => ({
+  type: 'SET_EXPENSES',
+  expenses
+});
+
+export const startSetExpenses = () => {
+  return (dispatch) => {
+    return database.ref('expenses').once('value').then((snapshot) => {
+      const expenses = [];
+
+      snapshot.forEach((childSnapshot) => {
+        expenses.push({
+          id: childSnapshot.key,
+          ...childSnapshot.val()
+        });
+      });
+
+      dispatch(setExpenses(expenses));
+    })
+  };
+};
+
+// export const startAddExpense = (expenseData = {}) => {
+//   return (dispatch) => {
+//     const {
+//       description = '',
+//       note = '',
+//       amount = 0,
+//       createdAt = 0
+//     } = expenseData;
+//     const expense = { description, note, amount, createdAt };
+//
+//     return database.ref('expenses').push(expense).then((rdef) => {
+//       dispatch(addExpense({
+//         id: rdef.key,
+//         ...expense
+//       }))
+//     });
+//   };
+// };
+//
+//
+//
+// database.ref('expenses')
+//   .once('value')
+//   .then((snapshot) => {
+//     const expenses = [];
+//
+//     snapshot.forEach((childSnapshot) => {
+//       expenses.push({
+//         id: childSnapshot.key,
+//         ...childSnapshot.val()
+//       });
+//     });
+//     console.log(expenses);
+//   }
+// );
